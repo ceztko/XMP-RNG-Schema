201c68428afe12d1fbf2293f59f86e5398473aaa
Francesco Pretto <ceztko@gmail.com>
2025-07-16 23:55:01 +0200

    Rename solution to XMP_RNG_Suite

--- generate-schemas.ps1
+++ generate-schemas.ps1
@@ -3,5 +3,5 @@
 $PSNativeCommandUseErrorActionPreference = $true
 $ErrorActionPreference = 'stop'
 
-dotnet build -c Release "/p:Platform=Any CPU" "$PSScriptRoot/RNGMerger/RNGMerger.sln"
-. "$PSScriptRoot/RNGMerger/bin/Release/RNGMerger" --input "$PSScriptRoot/Schemas/XMP_FullPacket.rng" --outdir "$PSScriptRoot"
+dotnet build -c Release "/p:Platform=Any CPU" "$PSScriptRoot/XMP_RNG_Suite/XMP_RNG_Suite.sln"
+. "$PSScriptRoot/XMP_RNG_Suite/bin/Release/RNGMerger" --input "$PSScriptRoot/Schemas/XMP_FullPacket.rng" --outdir "$PSScriptRoot"
